3f90c64f48f1939ba60db73c406ae55ff86b7f86
Jonathan Mani <jonathanmani@protonmail.com>
2022-08-29 16:01:01 -0400

    fixed some mongoose bugs and got connection working

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express()
 const mongoose = require('mongoose')
 const passport = require('passport')
 const session = require('express-session')
-const MongoStore = require('connect-mongo')(session)
+const MongoStore = require('connect-mongo')
 const flash = require('express-flash')
 const logger = require('morgan')
 const connectDB = require('./config/database')
@@ -29,13 +29,15 @@ app.use(
       secret: 'keyboard cat',
       resave: false,
       saveUninitialized: false,
-      store: new MongoStore({ mongooseConnection: mongoose.connection }),
+      store: MongoStore.create({
+        mongoUrl: process.env.DB_STRING,
+        mongooseConnection: mongoose.connection }),
     })
 )
 
 app.use(flash())
   
-app.use('/', mainRoutes)
+// app.use('/', mainRoutes)
 
 //Start Server
 app.listen(process.env.PORT, ()=>{
