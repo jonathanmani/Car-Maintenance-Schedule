89bd869172fcda407d8c5f4665174bddff520dda
Jonathan Mani <jonathanmani@protonmail.com>
2022-08-30 11:33:07 -0400

    added update and delete methods to the maintenance controller

--- controllers/maintenance.js
+++ controllers/maintenance.js
@@ -10,7 +10,7 @@ module.exports = {
         user: req.user,
       });
     } catch (err) {
-      console.error(err);
+      console.log(err);
     }
   },
   createMaintenance: async (req, res) => {
@@ -26,7 +26,36 @@ module.exports = {
       console.log("Maintenance Item Created");
       res.redirect("/maintenance");
     } catch (err) {
-      console.error(err);
+      console.log(err);
     }
   },
+  changeMaintence: async (req, res) => {
+    try {
+      await Maintenance.findOneAndUpdate(
+        { _id: req.body.todoIdFromJSFile },
+        {
+          serviceType: req.body.serviceType,
+          datePerformed: req.body.datePerformed,
+          location: req.body.location,
+          nextDate: req.body,
+          status: req.body.status,
+          userId: req.user.id,
+        }
+        console.log('Maintenance Updated')
+        res.json('Maintenance Updated')
+      );
+    } catch (err) {
+      console.log(err);
+    }
+  },
+  deleteMaintenance: async (req, res) => {
+    console.log(req.body.todoIdFromJSFile)
+    try {
+      await Todo.findOneAndDelete({_id:req.body.todoIdFromJSFile})
+      console.log('Deleted Maintenance')
+      res.json('Deleted Maintenance')
+    } catch (err) {
+      console.log(err)
+    }
+  }
 };
